1d48f5e3e8c62c7265b2021d0561be2e684a7fab
josephkoop <144931956+josephkoop@users.noreply.github.com>
2025-03-26 19:27:12 +0000

    Combined function

--- main.go
+++ main.go
@@ -8,37 +8,34 @@ import (
 	"sync"
 )
 
-func dialMachine(i int, wg *sync.WaitGroup){
+func main(){
 	target := "scanme.nmap.org"
 	dialer := net.Dialer{
 		Timeout: 5 * time.Second,
 	}
 	maxRetries := 3
-	portStr := strconv.Itoa(i)
-	address := net.JoinHostPort(target, portStr)
-
-	for j := 0; j < maxRetries; j++ {
-		conn, err := dialer.Dial("tcp", address)
-		if err == nil {
-			defer conn.Close()
-			fmt.Printf("Connection to %s was successful\n", address)
-			break
-		}
-		backoff := time.Duration(1<<j) * time.Second
-		fmt.Printf("Port %d Attempt %d failed. Waiting %v...\n", i, j+1, backoff)
-		time.Sleep(backoff)
-	}
-
-	defer wg.Done()
-}
-
-func main(){
 	port := 1
 	maxPort := 90
 	var wg sync.WaitGroup
-	wg.Add(maxPort)
-	for i := port; i < maxPort; i++ {
-		go dialMachine(i, &wg)
+	
+	for i := port; i <= maxPort; i++ {
+		portStr := strconv.Itoa(i)
+		address := net.JoinHostPort(target, portStr)
+		wg.Add(1)
+		go func (address string){
+			for j := 0; j < maxRetries; j++ {
+				conn, err := dialer.Dial("tcp", address)
+				if err == nil {
+					defer conn.Close()
+					fmt.Printf("Connection to %s was successful\n", address)
+					break
+				}
+				backoff := time.Duration(1<<j) * time.Second
+				fmt.Printf("Port %d Attempt %d failed. Waiting %v...\n", i, j+1, backoff)
+				time.Sleep(backoff)
+			}
+			defer wg.Done()
+		}(address)
 	}
 	wg.Wait()
 }
